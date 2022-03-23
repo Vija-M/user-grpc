4df3f4170cd670133ee6ebbb330a52c8b01039a3
vija.markule <vija.markule@accenture.com>
2022-03-23 20:20:38 +0200

    registration starts

--- src/main/java/service/RegistrationService.java
+++ src/main/java/service/RegistrationService.java
@@ -1,4 +1,9 @@
 package service;
 
+import com.proto.user.User;
+
 public class RegistrationService {
+    public boolean register (User user) {
+        return true;
+    }
 }
---
+++ src/test/java/service/RegistrationServiceTest.java
@@ -0,0 +1,18 @@
+package service;
+
+import com.proto.user.User;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.jupiter.api.Assertions;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class RegistrationServiceTest {
+    RegistrationService subject = new RegistrationService();
+
+     @Test
+    public void test () {
+      //  assertEquals(true, subject.register(User.getDefaultInstance()));
+
+    }
+}
