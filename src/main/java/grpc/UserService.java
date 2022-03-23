850dcc443046ed21f774806752b9d0387763ea33
vija.markule <vija.markule@accenture.com>
2022-03-23 17:06:12 +0200

    userService

--- src/main/java/grpc/UserService.java
+++ src/main/java/grpc/UserService.java
@@ -1,14 +1,17 @@
-package service;
+package grpc;
 
 import com.proto.user.UserRequest;
 import com.proto.user.UserResponse;
-import com.proto.user.User_serviceGrpc;
+import com.proto.user.UserServiceGrpc;
 import io.grpc.stub.StreamObserver;
 
-public class UserService extends User_serviceGrpc.User_serviceImplBase {
+public class UserService extends UserServiceGrpc.UserServiceImplBase {
     @Override
     public void userCreation(UserRequest request, StreamObserver<UserResponse> responseObserver) {
        // super.userCreation(request, responseObserver);
         System.out.println("Let's start user registration" + request.toString());
+        UserResponse response = UserResponse.newBuilder().setCreated(true).build();
+        responseObserver.onNext(response);
+        responseObserver.onCompleted();
     }
  }
