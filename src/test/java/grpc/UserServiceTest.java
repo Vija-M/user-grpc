7690177cc2bf7ae772cb6359ca76104da8ed6c55
vija.markule <vija.markule@accenture.com>
2022-03-29 10:23:14 +0300

    Adding Users Service Test

--- src/test/java/grpc/UserServiceTest.java
+++ src/test/java/grpc/UserServiceTest.java
@@ -2,14 +2,32 @@ package grpc;
 
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
-
+import org.junit.jupiter.api.TestInstance;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.mockito.junit.jupiter.MockitoExtension;
+import service.RegistrationService;
 import static org.junit.jupiter.api.Assertions.*;
 
+@ExtendWith(MockitoExtension.class)
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+
 class UserServiceTest {
+    @InjectMocks
+    private UserService service;
+
+    @Mock
+    private RegistrationService registrationService;
 
     @BeforeEach
-    void setUp() {
+    public void setUp() {
+        MockitoAnnotations.openMocks(this);
     }
+
+
+
 
     @AfterEach
     void tearDown() {
