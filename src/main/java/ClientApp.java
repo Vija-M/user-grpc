da7c5d0a3f6af6ded4c9e3c6621b9f1736d31b97
vija.markule <vija.markule@accenture.com>
2022-03-31 18:19:05 +0300

    Mutual TLS updates

--- src/main/java/ClientApp.java
+++ src/main/java/ClientApp.java
@@ -4,9 +4,14 @@ import com.proto.user.UserResponse;
 import com.proto.user.UserServiceGrpc;
 import io.grpc.ManagedChannel;
 import io.grpc.StatusRuntimeException;
+import io.grpc.netty.GrpcSslContexts;
+import io.grpc.netty.NettyChannelBuilder;
 import io.grpc.okhttp.OkHttpChannelBuilder;
 import io.grpc.stub.StreamObserver;
+import io.netty.handler.ssl.SslContext;
 
+import javax.net.ssl.SSLException;
+import java.io.File;
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
@@ -17,37 +22,62 @@ public class ClientApp {
     private static final String host = "localhost";
     private static final int port = 9090;
     private static ManagedChannel channel;
+    private static ManagedChannel mtlsChannel;
     private static UserServiceGrpc.UserServiceBlockingStub blockingStub;
     private static UserServiceGrpc.UserServiceStub nonBlockingStub;
+    private static UserServiceGrpc.UserServiceBlockingStub mtlsBlockingStub;
+    private static UserServiceGrpc.UserServiceStub mtlsNonBlockingStub;
     private static List<UserRequest> userRequestList;
     private static UserRequest userRequest;
+    private static boolean sslConnection = true;
 
-    static {
+    public ClientApp() {
+        System.out.println("simple created");
         channel = OkHttpChannelBuilder.forAddress(host, port)
-                .usePlaintext()
-                .build();
+                .usePlaintext().build();
         blockingStub = UserServiceGrpc.newBlockingStub(channel);
         nonBlockingStub = UserServiceGrpc.newStub(channel);
-        userRequestList = createUserRequestList();
-        userRequest = createUserRequest();
+ //       userRequestList = createUserRequestList();
+   //     userRequest = createUserRequest();
+    }
+
+    public ClientApp(SslContext sslContext) {
+        System.out.println("ssl created");
+        mtlsChannel = NettyChannelBuilder.forAddress(host, port)
+                .sslContext(sslContext).build();
+        mtlsBlockingStub = UserServiceGrpc.newBlockingStub(mtlsChannel);
+        mtlsNonBlockingStub = UserServiceGrpc.newStub(mtlsChannel);
     }
 
 
-    public static void main(String[] args) throws InterruptedException {
+    public static void main(String[] args) throws InterruptedException , SSLException {
+        if(args.length==1) sslConnection = args[0].equals("true")? true : false;
+
+        if(!sslConnection) {
+        new ClientApp();
         callToServer(userRequest);
         serverSideStream(userRequest);
         userSideStream(userRequestList);
         bidirectStream(userRequestList);
-    }
-
+        } else {
+            SslContext sslContext = ClientApp.loadTLSCredentials();
+            new ClientApp(sslContext);
+            mtlsCallToServer(userRequest);
+    }}
 
     public static UserResponse callToServer(UserRequest request) {
-        System.out.println("Unidirectional - Created user will be sent over: " + request);
+        System.out.println("Unidirectional: created user will be sent over: " + request);
         UserResponse response = blockingStub.register(request);
-        System.out.println("Unidirectional - User registered: " + response.getCreated());
+        System.out.println("Unidirectional: registered user: " + response.getCreated());
         return response;
     }
 
+    public static UserResponse mtlsCallToServer(UserRequest request) {
+        System.out.println("Unidirectional (mTLS): created user will be sent over: " + request);
+        UserResponse response = mtlsBlockingStub.register(request);
+        System.out.println("Unidirectional (mTLS): registered user: " + response.getCreated());
+        return response;
+    }
 
     public static void serverSideStream(UserRequest request) {
         Iterator<UserResponse> responses;
@@ -71,18 +101,18 @@ public class ClientApp {
             @Override
             public void onNext(UserResponse allRegistered) {
                 allDone[0] = allRegistered.getCreated();
-                System.out.println("Client side streaming - all users registered? " + allRegistered.getCreated());
+                System.out.println("User-side streaming - created users " + allRegistered.getCreated());
             }
 
             @Override
             public void onCompleted() {
-                System.out.println("Client side streaming completed");
+                System.out.println("User-side streaming is done");
                 finishLatch.countDown();
             }
 
             @Override
-            public void onError(Throwable t) {
-                System.out.println("Error " + t.getLocalizedMessage());
+            public void onError(Throwable thr) {
+                System.out.println("Error " + thr.getLocalizedMessage());
                 finishLatch.countDown();
             }
         };
@@ -90,20 +120,20 @@ public class ClientApp {
         StreamObserver<UserRequest> requestObserver = nonBlockingStub.userStreamCreateUsers(responseObserver);
         try {
 
-            for (UserRequest r : userRequestList) {
-                System.out.println("User to be sent over: " + r.getUser());
-                requestObserver.onNext(r);
+            for (UserRequest req : userRequestList) {
+                System.out.println("User to be sent over: " + req.getUser());
+                requestObserver.onNext(req);
                 if (finishLatch.getCount() == 0) {
                     return allDone[0];
                 }
             }
-        } catch (RuntimeException e) {
-            requestObserver.onError(e);
-            throw e;
+        } catch (RuntimeException exc) {
+            requestObserver.onError(exc);
+            throw exc;
         }
         requestObserver.onCompleted();
         if (!finishLatch.await(1, TimeUnit.MINUTES)) {
-            System.out.println("1 minute is not enough to finish process");
+            System.out.println("1 minute is not enough for to finish process");
         }
         return allDone[0];
     }
@@ -172,4 +202,16 @@ public class ClientApp {
     }
 
 
+
+    public static SslContext loadTLSCredentials() throws SSLException {
+        System.out.println("ssl loaded");
+        File serverCACertFile = new File("certif/ca-cert.pem");
+        File clientCertFile = new File("certif/client-cert.pem");
+        File clientKeyFile = new File("certif/client-key.pem");
+
+        return GrpcSslContexts.forClient()
+                .keyManager(clientCertFile, clientKeyFile)
+                .trustManager(serverCACertFile)
+                .build();
+    }
 }
--- src/main/java/Server.java
+++ src/main/java/Server.java
@@ -1,18 +1,60 @@
 import grpc.UserService;
-import io.grpc.ServerBuilder;
+import io.grpc.*;
+import io.grpc.netty.GrpcSslContexts;
+import io.grpc.netty.NettyServerBuilder;
+import io.netty.handler.ssl.ClientAuth;
+import io.netty.handler.ssl.SslContext;
+import io.netty.handler.ssl.SslContextBuilder;
 
+import javax.net.ssl.SSLException;
+import java.io.File;
 import java.io.IOException;
 
 public class Server {
+    private static final int port = 9090;
+    private static io.grpc.Server server;
+    private static boolean sslConnection = true;  // ssl or simple connection
 
     public static void main(String[] args) throws IOException, InterruptedException {
-        io.grpc.Server server = ServerBuilder
-                .forPort(9090)
-                .addService(new UserService()).build();
-
-        server.start();
-        System.out.println("Server started");
+        if (args.length == 1) sslConnection = args[0].equals("true") ? true : false;
+        if (!sslConnection) {
+            simpleServerStart();
+        } else {
+            mtlsServerStart(); //mutual TLS
+        }
         server.awaitTermination();
     }
 
+    public static void simpleServerStart() throws IOException, InterruptedException {
+        server = ServerBuilder.forPort(port).addService(new UserService()).build();
+        server.start();
+        System.out.println("Server started");
+    }
+
+    public static void mtlsServerStart() throws IOException, InterruptedException {
+        SslContext sslContext = Server.loadTLSCredentials();
+
+        server = NettyServerBuilder.forPort(9090).sslContext(sslContext)
+                .addService(new UserService()).build();
+        server.start();
+
+        System.out.println("mTLS server started");
+    }
+
+    public static void shutdownServer() {
+        server.shutdown();
+    }
+
+    public static SslContext loadTLSCredentials() throws SSLException {
+        File serverCertFile = new File("certif/server-cert.pem");
+        File serverKeyFile = new File("certif/server-key.pem");
+        File clientCACertFile = new File("certif/ca-cert.pem");
+
+        SslContextBuilder ctxBuilder = SslContextBuilder.forServer(serverCertFile, serverKeyFile)
+                .clientAuth(ClientAuth.REQUIRE).trustManager(clientCACertFile);
+
+        return GrpcSslContexts.configure(ctxBuilder).build();
+    }
 }
+
+
